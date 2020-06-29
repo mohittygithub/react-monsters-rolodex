8fefa63a13eba6d84e7c25b2befcdbcbecfb2650
mohittygithub <smartfriendmohit@gmail.com>
2020-06-29 09:05:41 +0530

    Updates

---
+++ precache-manifest.d3c5a37825bedcfc3fc3918666565789.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "88e7ace4480d786b0f278f0631766f61",
+    "url": "/react-monsters.rolodex/index.html"
+  },
+  {
+    "revision": "9c8d6af2c3f1578ce913",
+    "url": "/react-monsters.rolodex/static/css/main.242f5e69.chunk.css"
+  },
+  {
+    "revision": "a2e4be9481a54b890f97",
+    "url": "/react-monsters.rolodex/static/js/2.7aaef059.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/react-monsters.rolodex/static/js/2.7aaef059.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "9c8d6af2c3f1578ce913",
+    "url": "/react-monsters.rolodex/static/js/main.820c6e6d.chunk.js"
+  },
+  {
+    "revision": "24b39550e9c7b789c05f",
+    "url": "/react-monsters.rolodex/static/js/runtime-main.93cdc53c.js"
+  }
+]);
